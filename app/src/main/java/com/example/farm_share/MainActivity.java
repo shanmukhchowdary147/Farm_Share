c6a5cb078707c2d63e2b7e46d89b7e236234b05a
shanmukhchowdary147 <shanmukhchowdary147@gmail.com>
2020-10-22 20:04:52 +0530

    Farm Share Solutions

--- app/src/main/java/com/example/farm_share/MainActivity.java
+++ app/src/main/java/com/example/farm_share/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.farm_share;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,18 +9,35 @@ import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public CardView cardEditprofile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardEditprofile=(CardView) findViewById(R.id.cEditProfile);
+        cardEditprofile.setOnClickListener(this);
     }
+
 
 
     public void logout(View view) {
             FirebaseAuth.getInstance().signOut(); //logout
             startActivity(new Intent(getApplicationContext(),Login.class));
             finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            case R.id.cEditProfile :
+                i=new Intent(this,EditProfile.class);
+                startActivity(i);
+                break;
+        }
+
     }
 }
