2f1d192f351219bb1332dd4c53e345842b6deb46
shanmukhchowdary147 <shanmukhchowdary147@gmail.com>
2020-10-24 12:19:49 +0530

    Farm Share Solutions

--- app/src/main/java/com/example/farm_share/MainActivity.java
+++ app/src/main/java/com/example/farm_share/MainActivity.java
@@ -12,22 +12,34 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public CardView cardEditprofile;
+    public CardView cardSearch;
+    public CardView cardAdd;
+    public CardView cardActivity;
+    public CardView cardFav;
+    public CardView cardLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cardEditprofile=(CardView) findViewById(R.id.cEditProfile);
+        cardSearch=(CardView) findViewById(R.id.cSearch);
+        cardAdd=(CardView) findViewById(R.id.cAdd);
+        cardActivity=(CardView) findViewById(R.id.cActivity);
+        cardFav=(CardView) findViewById(R.id.cFav);
+        cardLogout=(CardView) findViewById(R.id.cLogout);
+
         cardEditprofile.setOnClickListener(this);
+        cardSearch.setOnClickListener(this);
+        cardAdd.setOnClickListener(this);
+        cardActivity.setOnClickListener(this);
+        cardFav.setOnClickListener(this);
+        cardLogout.setOnClickListener(this);
     }
 
 
 
-    public void logout(View view) {
-            FirebaseAuth.getInstance().signOut(); //logout
-            startActivity(new Intent(getApplicationContext(),Login.class));
-            finish();
-    }
+    
 
     @Override
     public void onClick(View v) {
@@ -35,6 +47,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.cEditProfile :
                 i=new Intent(this,EditProfile.class);
+                startActivity(i);
+                break;
+            case R.id.cAdd :
+                i=new Intent(this,Add_Equip.class);
+                startActivity(i);
+                break;
+            case R.id.cLogout :
+                FirebaseAuth.getInstance().signOut(); //logout
+                i=new Intent(this,Login.class);
                 startActivity(i);
                 break;
         }
