39315694567425fc2ef36e095e2e26c4381cc277
hemanth379 <hemanth.varma379@gmail.com>
2020-10-27 14:25:40 +0530

    1

--- app/src/main/java/com/example/farm_share/Add_Equip.java
+++ app/src/main/java/com/example/farm_share/Add_Equip.java
@@ -3,12 +3,19 @@ package com.example.farm_share;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Add_Equip extends AppCompatActivity {
 
+    private String CategoryName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__equip);
+
+        CategoryName = getIntent() .getExtras().get("category").toString();
+
+        Toast.makeText(this, CategoryName, Toast.LENGTH_SHORT).show();
+
     }
 }
