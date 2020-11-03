893580c87f3ff87486b4dc8d6e9fa430b70a940e
shanmukhchowdary147 <shanmukhchowdary147@gmail.com>
2020-11-03 18:42:48 +0530

    Farm Share Solutions

--- app/src/main/java/com/example/farm_share/Otp.java
+++ app/src/main/java/com/example/farm_share/Otp.java
@@ -1,7 +1,9 @@
 package com.example.farm_share;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +12,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.farm_share.Model.Users;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.TimeUnit;
 
@@ -44,21 +55,91 @@ public class Otp extends AppCompatActivity {
         Verify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!Vphone.getText().toString().isEmpty() && Vphone.getText().toString().length()==10)
+                if (!verficationInProgress)
                 {
-                    progressBar.setVisibility(View.VISIBLE);
-                    Vlogin.setText("Sending OTP..");
-                    Vlogin.setVisibility(View.VISIBLE);
-                    requestOTP(Vphone);
+                    if(!Vphone.getText().toString().isEmpty() && Vphone.getText().toString().length()==10)
+                    {
+                        progressBar.setVisibility(View.VISIBLE);
+                        Vlogin.setText("Sending OTP..");
+                        Vlogin.setVisibility(View.VISIBLE);
+                        requestOTP(Vphone);
+
+                    }
+                    else
+                    {
+                        Vphone.setError("Phone Number is Not Valid");
+                    }
                 }
                 else
                 {
-                    Vphone.setError("Phone Number is Not Valid");
+                    String userotp=Votp.getText().toString();
+                    if(!userotp.isEmpty() && userotp.length()==6)
+                    {
+                        PhoneAuthCredential credential= PhoneAuthProvider.getCredential(verificationId,userotp);
+                        verifyAuth(credential);
+                    }
+                    else
+                    {
+                        Votp.setError("Enter Valid OTP");
+                    }
+
+
                 }
+
             }
         });
 
 
+    }
+
+    private void verifyAuth(PhoneAuthCredential credential) {
+        fAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(Otp.this, "Phone Number is verified", Toast.LENGTH_SHORT).show();
+                    Redirect(Vphone);
+                }
+                else
+                {
+                    Toast.makeText(Otp.this, "Authentication failed!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void Redirect(EditText vphone) {
+        final String SVphone=Vphone.getText().toString();
+        final DatabaseReference RootRef;
+        RootRef= FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child("Users").child(SVphone).exists())
+                {
+                    Users userData= dataSnapshot.child("Users").child(SVphone).getValue(Users.class);
+                    if (userData.getPhone().equals(SVphone))
+                    {
+                            Toast.makeText(Otp.this, "Login Success..", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            progressBar.setVisibility(View.GONE);
+                    }
+
+                }
+                else
+                {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(Otp.this, "Create new Account", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),Register.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void requestOTP(EditText Vphone) {
@@ -68,12 +149,12 @@ public class Otp extends AppCompatActivity {
             public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
                 super.onCodeSent(s, forceResendingToken);
                 progressBar.setVisibility(View.GONE);
-                Vlogin.setVisibility(View.GONE);
                 Votp.setVisibility(View.VISIBLE);
                 verificationId = s;
                 token = forceResendingToken;
                 Verify.setText("Verify");
                 Verify.setEnabled(false);
+                verficationInProgress= true;
 
 
             }
