ced5b281b9e301ca318fb7ac99d2a97f4762c11c
hemanth379 <hemanth.varma379@gmail.com>
2020-11-05 21:42:09 +0530

    1

--- app/src/main/java/com/example/farm_share/Otp.java
+++ app/src/main/java/com/example/farm_share/Otp.java
@@ -73,7 +73,7 @@ public class Otp extends AppCompatActivity {
                         requestOTP(Vphone);
 
                     }
-                    else
+                     else
                     {
                         Vphone.setError("Phone Number is Not Valid");
                     }
